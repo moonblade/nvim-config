6395d00eb042b98d25c69fb53e7d122d1c6cb942
moonblade <moonblade168@gmail.com>
2023-05-18 09:27:29 +0530

    add gruvbox and git gutter

---
+++ lua/package-config/gruvbox.lua
@@ -0,0 +1,6 @@
+require("gruvbox").setup({
+})
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
+
--- lua/package-config/mason-lspconfig.lua
+++ lua/package-config/mason-lspconfig.lua
@@ -14,3 +14,17 @@ require("mason-lspconfig").setup_handlers({
         require("lspconfig")[server_name].setup {}
     end,
 })
+
+-- lsp config
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  end
+})
+
