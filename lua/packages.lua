88ab3ca2e742b182a5d09363d9c527a3d7224f97
moonblade <moonblade168@gmail.com>
2024-02-11 09:36:18 +0530

    change telescope version

--- lua/packages.lua
+++ lua/packages.lua
@@ -144,7 +144,7 @@ require("packer").startup({
 
     -- Telescope
     use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      'nvim-telescope/telescope.nvim', tag = '0.1.4',
       -- 'nvim-telescope/telescope.nvim',
       requires = {
         {'nvim-lua/plenary.nvim'},
